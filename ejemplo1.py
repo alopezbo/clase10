636ca599255b7fc029801b1cbd6a2a9f8bbd31bd
alopezbo <andlobo19@yahoo.com>
2019-10-17 20:31:19 -0600

    Segundo Commit

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,14 +1,36 @@
+# from mechanize import Browser
+# import csv
+# with open('namecsv.csv') as csvfile: #forma correcta de abrir un archivo
+#     reader=csv.DictReader(csvfile)
+#     for rows in csvfile:
+#         browser=Browser()   #crear variable Browser
+#         browser.open("https://www.tse.go.cr/consulta_persona/consulta_cedula.aspx")
+#         form=browser.select_form("form1")
+#         browser["txtcedula"]=rows
+#         response=browser.submit()
+#         name=str(rows)
+#         file=open(name+".html","w")                    #Forma incorrecta
+#         file.write(response.read().decode("utf-8"))     #de abrir
+#         file.close()                                    #un archivo
+
+
+#EJEMPLO 2
 from mechanize import Browser
 import csv
-with open ('namecsv.csv') as csvfile:
-    reaver=csv.DictReader(csvfile)
+with open('namecsv.csv') as csvfile: #forma correcta de abrir un archivo
+    reader=csv.DictReader(csvfile)
     for rows in csvfile:
-        browser=Browser()
-        browser.open("https://www.tse.go.cr/consulta_persona/consulta_cedula.aspx")
+        browser=Browser()   #crear variable Browser
+        browser.open("https://www.tse.go.cr/consulta_persona/consulta_nombres.aspx")
         form=browser.select_form("form1")
-        browser["texcedula"]=rows
+        browser["txtcedula"]=rows
         response=browser.submit()
         name=str(rows)
-        file=open("+name+.html","w")
-        file.write(response.read())
-        file.close()
+        file=open(name+".html","w")                    #Forma incorrecta
+        file.write(response.read().decode("utf-8"))     #de abrir
+        file.close()                                    #un archivo
+
+
+        txtnombre
+        txtapellido1
+        txtapellido2
